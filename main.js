542f3ccd12875e2a295e62b6e9793b1a55f6fae4
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-07-10 18:17:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+function preload(){
+
+}
+
+function setup(){
+   canvas= createCanvas(400,400);
+   canvas.center();
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save('mustasche-deleto.png');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
