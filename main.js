63bf459d778a9e58fbc9f5b5bb545310188d11a6
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-07-11 21:28:56 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 function preload(){
-
 }
-
 function setup(){
    canvas= createCanvas(400,400);
    canvas.center();
 }
-
 function draw(){
-
 }
 function take_snapshot(){
     save('mustasche-deleto.png');
