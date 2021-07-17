85b7fa3009d3caaf9c5e5fc19e87cc06c75be0bf
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-07-17 17:14:17 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
+noseX = 0;
+noseY = 0;
+
+
+
+
 function preload(){
-
+    mustache_img = loadImage('https://i.postimg.cc/ZnBKsbsj/7-2-moustache-png-image-thumb.png')
 }
-
 function setup(){
-   canvas= createCanvas(300,300);
+   canvas= createCanvas(400,400);
    canvas.center();
-   //new//
+   //wecam setup here long.......//
    video = createCapture(VIDEO);
-   video.size(300,300);
+   video.size(400,400);
    video.hide();
 
    poseNet = ml5.poseNet(video,modelLoaded);
    poseNet.on('pose',gotPoses);
 }
-
 function draw(){
-image(video ,0,0,300,300);
+    image(video ,0,0,400,400);
+    image(mustache_img,noseX,noseY,80,40);
 }
 function take_snapshot(){
-    save('deleto.jpg');
+    save('mustasche-deleto.png');
+}
+function modelLoaded(){
+console.log("poseNet model iSss activated , initialized and ready to GO");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y= " + results[0].pose.nose.y);
+    }
+   
+}
 
-
-
-
-
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
 
 
 
@@ -59,16 +81,3 @@ function take_snapshot(){
 
 
     
-}
-
-function modelLoaded(){
-    console.log("Posenet ISss actived and initialized");
-}
-
-function gotPoses(results){
-if(results.length > 0){
-    console.log(results);
-    console.log("nose x =" + results[0].pose.nose.x);
-    console.log("nose y= " + results[0].pose.nose.y);
-}
-}
